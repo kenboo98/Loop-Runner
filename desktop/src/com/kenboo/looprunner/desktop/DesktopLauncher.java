4e6d3fed825b7cdbb8f862a1b8096cfc9b8f8f5a
kenboo98 <kenboo1998@gmail.com>
2017-05-07 04:30:40 -0600

    Test commit. One minor change

---
+++ desktop/src/com/kenboo/looprunner/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.kenboo.looprunner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kenboo.looprunner.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 1280;
+		config.width = 720;
+		config.samples = 8;
+		new LwjglApplication(new MainGame(), config);
+	}
+}
